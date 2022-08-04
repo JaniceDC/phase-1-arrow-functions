05a66b0fa0776b8217db8e31ba8fdd025c41a4cc
JaniceDC <janice.dipayan.chen@gmail.com>
2022-08-04 10:59:30 +1000

    Completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide() {
+    return 2000/100
+}
+
+const square = x => x**2;
+
+const add = (parameter1, parameter2) =>{
+    console.log (`add ${parameter1}`);
+    console.log (`add ${parameter2}`);
+    return parameter1 + parameter2;
+}
+add(1,2);
